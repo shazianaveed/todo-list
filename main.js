365e340f9e81c78f2e9fc003e0fb34af1e30e864
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-09-15 20:35:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer_1.default.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            message: "What you want to add in your Todos ?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "Do you want to add more ?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
